a2cf1a1f6e01416f724cded8596b1cc243d03ab8
刘德威 <2601469600@qq.com>
2024-09-08 13:51:40 +0800

    更新-同步

--- my-new-project/src/views/AboutView.vue
+++ my-new-project/src/views/AboutView.vue
@@ -2,7 +2,7 @@
   <div class="about">
     <h1>This is an about page</h1>
     <el-row>
-      <el-button>默认按钮</el-button>
+      <el-button>默认按钮 111</el-button>
       <el-button type="primary">主要按钮</el-button>
       <el-button type="success">成功按钮</el-button>
       <el-button type="info">信息按钮</el-button>
